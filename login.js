45781afc554d0575dc376498fedf49a28a16a7b8
ranflir <ranflir.sc@gmail.com>
2025-12-01 15:32:29 +0900

    Refactor: Improve login logic on main

--- login.js
+++ login.js
@@ -1,2 +1,2 @@
-// 수정 전 : console.log("Login logic is here");
-console.log("Login logic is complete");
+// 수정 전 : console.log("Login logic is here");  -> 수정 후 : console.log("Login logic is complete");  //  This time lets' make conflict!
+console.log("Main branch login logic");
